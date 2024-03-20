580efa80306752aeb2852e080f8b705cd61bc4f6
calenzo <calenzodevs@gmail.com>
2024-03-20 16:15:03 -0300

    feat: I was added the about

--- src/views/pages/Home/index.js
+++ src/views/pages/Home/index.js
@@ -3,5 +3,13 @@ import { Layout } from "views/components";
 import "./index.css";
 
 export const Home = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <div className="home">
+        <h1>Atto</h1>
+        <p>Started Date: 19/03/2024</p>
+        <p>Created By: Carlos Henrique Calenzo</p>
+      </div>
+    </Layout>
+  );
 };
